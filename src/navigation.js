ced6fff7512e29f26c333cc6e8ca91802cc01415
chris <ngmi@posteo.es>
2023-10-28 13:53:02 +0200

    updates

--- src/navigation.js
+++ src/navigation.js
@@ -37,7 +37,7 @@ export const headerData = {
 export const footerData = {
   secondaryLinks: [{ text: 'Terms', href: getPermalink('/terms') }],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://bsky.app/profile/einfachspanien.com' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
   ],
