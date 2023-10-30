089bcbbdfca489bf6d55de013845508109f64a97
chris <ngmi@posteo.es>
2023-10-30 09:19:54 +0100

    updates

--- src/navigation.js
+++ src/navigation.js
@@ -39,7 +39,11 @@ export const footerData = {
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://bsky.app/profile/einfachspanien.com' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/einfachspanien/' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    {
+      ariaLabel: 'Facebook',
+      icon: 'tabler:brand-facebook',
+      href: 'https://www.facebook.com/people/EinfachSpanien/61553085833174/',
+    },
   ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm]"></span>
